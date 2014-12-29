14fcf85308b9a016d202bb8d911cc48e9fd4a29c
Alex Wilson <aewilson@ualberta.ca>
2014-12-28 20:52:36 -0700

    Fix order of operations stuff

--- ast.js
+++ ast.js
@@ -8,18 +8,34 @@ module.exports = AST;
 var grammar = require('./grammar');
 
 var make_bin_op = function(name, ev) {
-    AST[name] = function(left, right) {
-        this.left = left; this.right = right;
+    AST[name] = function(right) {
+        this.right = right;
     };
 
     AST[name].prototype.Eval = ev;
 };
 
 
-make_bin_op('AddOp', function(s) { return this.left.Eval() + this.right.Eval(); });
-make_bin_op('SubOp', function(s) { return this.left.Eval() - this.right.Eval(); });
-make_bin_op('DivideOp', function(s) { return this.left.Eval() / this.right.Eval(); });
-make_bin_op('MultOp', function(s) { return this.left.Eval() * this.right.Eval(); });
+make_bin_op('AddOp', function(s, left) { return left + this.right.Eval(s); });
+make_bin_op('SubOp', function(s, left) { return left - this.right.Eval(s); });
+make_bin_op('DivideOp', function(s, left) { return left / this.right.Eval(); });
+make_bin_op('MultOp', function(s, left) { return left * this.right.Eval(); });
+
+
+AST.BinOpNode = function(left, ops) {
+    this.left = left;
+    this.ops = ops;
+}
+
+AST.BinOpNode.prototype.Eval = function(s) {
+    var left = this.left.Eval(s),
+        i = 0;
+
+    for (i = 0; i < this.ops.length; i++) {
+        left = this.ops[i].Eval(s, left);
+    }
+    return left; 
+}
 
 AST.Literal = function(val) { this.val = val; };
 AST.Literal.prototype.Eval = function(s) { return this.val; };
@@ -43,23 +59,17 @@ AST.Parser = function() {
         transform: function() { return new AST.Literal(this.textValue); },
     };
 
-    function bin_op_rs(ast, ls, rights, i) {
-        if (i >= rights.length) {
-            return ls.transform();
-        }
-        var op = rights[i].op.textValue
-            ,rs = rights[i].rs
-        ;
-        return new (ast[op])(ls.transform(), bin_op_rs(ast, rs, rights, i + 1));
-    };
-
-    function make_bin_op_parser(name, ast) {
+    function make_bin_op_parser(name, ops) {
         grammar.Parser[name] = {
             isa: name,
             transform: function() {
                 // example: 3+4+5+6
                 // ls:3 , elements[1][0] = {op:'+',rs: '4'}
-                return bin_op_rs(ast, this.ls, this.elements[1].elements, 0);
+                var rights = this.elements[1].elements.map(function(v) {
+                    return new (ops[v.op.textValue])(v.rs.transform());
+                });
+
+                return new AST.BinOpNode(this.ls.transform(), rights);
             }
         }
     };

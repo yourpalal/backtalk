0bb6ef409aad2749686537c2b503b0f0a7586818
Alex Wilson <aewilson@ualberta.ca>
2015-03-26 21:38:45 -0600

    Improve draw example

--- examples/draw/drawlib.js
+++ examples/draw/drawlib.js
@@ -1,6 +1,8 @@
 'use strict';
 
 window.Drawing = {
+    width: 0,
+    height: 0,
     Circle: function(size, color) {
         this.radius = {
             big: 30,
@@ -24,15 +26,50 @@ Drawing.Context = function() {
     this.gfx = [];
 };
 
+
+Drawing.makeObjectContext = function(object, scope) {
+    scope.addFunc({
+        patterns: ['a <bit|pixel> <left|right> of center',
+                   'at $ <pixel|pixels> <left|right> of center'],
+        impl: function() {
+            var how, much, lr, what = "center";
+            if (arguments.length == 2) {
+                how = arguments[0];
+                much = 1;
+                lr = arguments[1];
+            } else if (arguments.length == 3) {
+                much = arguments[0];
+                how = arguments[1];
+                lr = arguments[2];
+            }
+
+
+            if (how === 'bit') { much = 5 }
+            if (lr === 'left') { much *= -1 }
+            if (what === 'center') { what = Drawing.width / 2; }
+            
+            object.center[0] = what - much;
+        }
+    });
+};
+
 Drawing.Context.prototype.register = function(btcontext) {
     var ctx = this;
 
     btcontext.addFunc({
         patterns: ['a <small|big> <red|yellow|green|blue> circle'],
         impl: function(size, colour) {
-            ctx.gfx.push(new Drawing.Circle(size, colour));
+            var circle = new Drawing.Circle(size, colour);
+            ctx.gfx.push(circle);
+
+            Drawing.makeObjectContext(circle, this.scope);
+
+            if (this.newSubEval) {
+                this.eval(this.body);
+            }
         }
     });
+
 };
 
 Drawing.Context.prototype.update = function(canvas, ctx) {

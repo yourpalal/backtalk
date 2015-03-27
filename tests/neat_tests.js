1e81d7228f5257562411001b6c8c452a96cea45e
Alex Wilson <aewilson@ualberta.ca>
2015-03-26 20:37:16 -0600

    Add for loop test thing

---
+++ tests/neat_tests.js
@@ -0,0 +1,38 @@
+var BT = require('../back_talker')
+    ,should = require('should')
+    ,sinon = require('sinon')
+;
+
+
+it('BackTalker can', function() {
+    var scope, evaluator;
+
+    before(function() {
+        scope = new BT.Scope();
+        evaluator = new BT.Evaluator(scope);
+    });
+
+
+    it("make loops", function() {
+        scope.addFunc({
+            patterns: ["for $! from $ to $"]
+            ,impl: function(v, low, high) {
+                var i = 0,
+                    results = [];
+
+                for (; low + i < high; i++) {
+                    this.scope.set(v.name, low + i);
+                    results.push(this.eval(this.body));
+                }
+                return results;
+            }
+        });
+
+        var result = evaluator.eval([
+        "for $i from 0 to 10:",
+        "   $i + 5"
+        ].join("\n"));
+
+        results.should.equal([5,6,7,8,9,10,11,12,13,14]);
+    });
+});

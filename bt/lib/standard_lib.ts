3223031bc8c4289134904bea985aeb17f74ca993
Alex Wilson <aewilson@ualberta.ca>
2015-12-15 16:14:46 -0700

    Switch standard library to use Library class

--- bt/lib/standard_lib.ts
+++ bt/lib/standard_lib.ts
@@ -1,5 +1,6 @@
 import {Evaluator} from "./evaluator";
 import {FuncParams, Immediate} from "./functions";
+import {Library} from "./library";
 import * as secure from "./secure";
 import {Expresser, StackExpresser, StateMachineExpresser} from "./expressers";
 
@@ -56,81 +57,96 @@ class WhenRunningExpresser implements Expresser {
     }
 }
 
-var funcs = [
-    { // assignment
-        patterns: ['with $!!:ref as $:val', 'with $!!:ref as:'],
-        impl: function(args: FuncParams, self: Evaluator) {
-            let val = args.named.val;
+export var library = Library.create()
+.func("with_as", ['with $!!:ref as $:val', 'with $!!:ref as:'])
+    .help("sets a variable to the given value")
+    .callsBody(Library.ONCE)
+    .impl((args: FuncParams, self: Evaluator) => {
+        let val = args.named.val;
 
-            if (args.body) {
-                val = self.makeSub().eval(args.body);
+        if (args.body) {
+            val = self.makeSub().eval(args.body);
+        }
+
+        return Immediate.resolve(val).then((value) => {
+            self.scope.set(args.named.ref.name, value);
+            return value;
+        });
+    })
+
+.func("list_of", ['list of:'])
+    .callsBody(Library.ONCE)
+    .help("returns a list of the result of each expression in the body")
+    .impl((args: FuncParams, self: Evaluator) => {
+        // StackExpresser calls 'push' on each result
+        var list = [];
+        self.evalExpressions(args.body, new StackExpresser(list));
+
+        return list;
+    })
+
+.func("item_of", ['item $:count of $:list'])
+    .help("gets an item from the list.")
+    .impl((args: FuncParams, self: Evaluator) => {
+        var count = args.getNumber("count");
+        return args.named.list[count - 1];
+    })
+
+.func("property_of", ['property $:name of $:obj'])
+    .help("gets a named property of an object")
+    .impl((args: FuncParams, self: Evaluator) => {
+        var name = args.getString("name");
+        var obj = args.getObject("obj");
+
+        return secure.getProperty(obj, name);
+    })
+
+.func("print", ['print $:arg'])
+    .help("prints the provided value")
+    .impl((args: FuncParams, self: Evaluator) => {
+        let env = self.scope.env as StdEnv;
+        env.stdout.write(args.named.arg);
+
+        return args.named.arg;
+    })
+
+.func("when", ['when :'])
+    .help(`Supports conditional execution. The first expression to evaluate
+        to true will have its then expression run.
+
+        when:
+            $a
+            then:
+                print "a"
+            $b
+            then:
+                print "b"
+            true:
+                print "c"
+            `)
+    .includes()
+        .func("then", ["then :"])
+            .help("runs the next statement only if the previous guard passed")
+        .done()
+    .impl((args: FuncParams, self: Evaluator) => {
+        let sub = self.makeSub();
+        let expresser = new StateMachineExpresser<WhenState>();
+        expresser.setState(new WhenGuardingExpresser(expresser));
+
+        sub.scope.addFunc(['then :'], function(a: FuncParams): any {
+            if (expresser.state.running) {
+                return sub.eval(a.body);
             }
+            return false;
+        });
+        sub.evalExpressions(args.body, expresser);
 
-            return Immediate.resolve(val).then((value) => {
-                self.scope.set(args.named.ref.name, value);
-                return value;
-            });
-        }
-    },
-    { // list constructor
-        patterns: ['list of:'],
-        impl: function(args: FuncParams, self: Evaluator) {
-            // StackExpresser calls 'push' on each result
-            var list = [];
-            self.evalExpressions(args.body, new StackExpresser(list));
-
-            return list;
-        }
-    },
-    { // list accessor
-        patterns: ['item $:count of $:list'],
-        impl: function(args: FuncParams, self: Evaluator) {
-            var count = args.getNumber("count");
-            return args.named.list[count - 1];
-        }
-    },
-    { // property accessor
-        patterns: ['property $:name of $:obj'],
-        impl: function(args: FuncParams, self: Evaluator) {
-            var name = args.getString("name");
-            var obj = args.getObject("obj");
-
-            return secure.getProperty(obj, name);
-        }
-    },
-    { // printer
-        patterns: ['print $:arg'],
-        impl: function(args: FuncParams, self: Evaluator) {
-            let env = self.scope.env as StdEnv;
-            env.stdout.write(args.named.arg);
-
-            return args.named.arg;
-        }
-    },
-    { // conditional execution
-        patterns: ['when :'],
-        impl: function(args: FuncParams, self: Evaluator) {
-            let sub = self.makeSub();
-            let expresser = new StateMachineExpresser<WhenState>();
-            expresser.setState(new WhenGuardingExpresser(expresser));
-
-            sub.scope.addFunc(['then :'], function(a: FuncParams): any {
-                if (expresser.state.running) {
-                    return sub.eval(a.body);
-                }
-                return false;
-            });
-            sub.evalExpressions(args.body, expresser);
-
-            return expresser.state.result;
-        }
-    }
-];
+        return expresser.state.result;
+    })
+    .library;
 
 export function inScope(scope) {
     scope.env.stdout = new ConsoleWriter();
-    funcs.forEach(function(f) {
-        scope.addFunc(f.patterns, f.impl);
-    });
+    library.addToScope(scope);
     return scope;
 }
